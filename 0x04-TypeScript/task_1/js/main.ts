1c2fdeb175b2870f8d9d34791756642d3f88e91e
MedBrk <150671350+med-ctrl@users.noreply.github.com>
2025-05-25 12:46:21 +0100

    Writing a class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,6 +1,6 @@
 interface Teacher {
-readonly  FirstName: string;
-readonly LastName: string;
+readonly  firstName: string;
+readonly lastName: string;
     fullTimeEmployee: boolean;
     yearsOfExperience?: number;
     location: string;
@@ -8,13 +8,69 @@ readonly LastName: string;
 }
 
 const user: Teacher = {
-    FirstName: "simo",
-    LastName: "smith",
+  firstName: "simo",
+  lastName: "smith",
   fullTimeEmployee: true,
   yearsOfExperience: 2,
   location: "Marrakech",
 
 }
+interface Directors extends Teacher {
+    numberOfReports: number;
+}
 
+// function printTeacher (firstName : string , lastName : string) : string{
+//   return `${firstName[0]}. ${lastName}`
+// }
+// console.log(printTeacher("simo", "smith"))
 
-console.log(user)
+interface PrintTeacherFunction {
+  (firstName: string , lastName: string): string;
+}
+
+const printTeacher: PrintTeacherFunction = (firstName , lastName) => {
+ return `${firstName[0]}. ${lastName}`
+}
+
+console.log(printTeacher("simo", "smith"))
+
+interface StudentClass {
+  firstName: string;
+  lastName: string;
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+// Interface for the class instance
+interface StudentClassInterface {
+  firstName: string;
+  lastName: string;
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+// Interface for the constructor
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+// Class that implements the instance interface
+class StudentClass implements StudentClassInterface {
+  constructor(public firstName: string, public lastName: string) {}
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+// Using the constructor interface
+const createStudent = (ctor: StudentConstructor, firstName: string, lastName: string): StudentClassInterface => {
+  return new ctor(firstName, lastName);
+};
+
+const student = createStudent(StudentClass, "Mohamed", "Bennani");
+console.log(student.displayName()); // Mohamed
